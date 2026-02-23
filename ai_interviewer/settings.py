0fa76a1bb32a7148807cbdbfd81baa7711b18fc0
Vanshika Jhala <vanshikajhala009@gmail.com>
2026-02-23 18:35:22 +0530

    Revert "Merge branch 'main' of https://github.com/Vanshikajhala44/ai-interviewer"
    This reverts commit 2d13bb153ac733d1c109ed7fd8c0e6b509c7eb4e, reversing
    changes made to 4e01ea02acb11bf0c20425511fc6de043eef424c.

--- ai_interviewer/settings.py
+++ ai_interviewer/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
@@ -139,6 +137,7 @@ import os
 
 load_dotenv()
 
+print("Loaded GROQ KEY:", os.getenv("GROQ_API_KEY"))
 import cloudinary
 import os
 
