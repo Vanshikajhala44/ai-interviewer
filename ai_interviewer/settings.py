4e01ea02acb11bf0c20425511fc6de043eef424c
Vanshika Jhala <vanshikajhala009@gmail.com>
2026-02-23 18:00:12 +0530

    change db

--- ai_interviewer/settings.py
+++ ai_interviewer/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +81,17 @@ WSGI_APPLICATION = "ai_interviewer.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -138,3 +147,5 @@ cloudinary.config(
     api_secret="ExBid5vylyt_bpwBpef4Zz_ii-A",  # your actual API secret
     secure=True
 )
+
+
