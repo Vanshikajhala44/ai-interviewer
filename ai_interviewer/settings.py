b17e2754131960f8d4051e4a4b81b8ca6f014f73
Maaz-khokhar <m.khokhar@braininventory.com>
2026-02-23 11:22:03 +0530

    changes pushed for deployement

--- ai_interviewer/settings.py
+++ ai_interviewer/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
@@ -128,7 +130,6 @@ import os
 
 load_dotenv()
 
-print("Loaded GROQ KEY:", os.getenv("GROQ_API_KEY"))
 import cloudinary
 import os
 
