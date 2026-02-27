a64049ea7e4a19c4a3786644628832b4c4ae21aa
Vanshika Jhala <vanshikajhala009@gmail.com>
2026-02-27 14:40:49 +0530

    Update settings.py

--- ai_interviewer/settings.py
+++ ai_interviewer/settings.py
@@ -144,8 +144,8 @@ cloudinary.config(
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
